c81f616e247e82d2c0fd51fca5a029669c24e2ff
Mohamed Aziz Ammar <108346635+aziz4ammar@users.noreply.github.com>
2023-05-16 21:19:02 +0100

    Final Commit

---
+++ Backspaces in string.py
@@ -0,0 +1,9 @@
+def clean_string(s):
+    stack = []
+    for char in s:
+        if char == '#':
+            if stack:
+                stack.pop()
+        else:
+            stack.append(char)
+    return ''.join(stack)
